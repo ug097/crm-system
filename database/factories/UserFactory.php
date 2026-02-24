6c46d501f3d58bc9c5804a56614b5fd42c3054e0
097yuji <g621308@gmail.com>
2026-02-24 16:08:42 +0900

    Factoryのfakerエラーを解消

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +23,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = $this->faker ?? FakerFactory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
